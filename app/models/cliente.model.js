4fc6a0614ba52f7ae38a889062943525b6a4800d
carlosscarranza <58747093+carlosscarranza@users.noreply.github.com>
2021-08-28 12:13:26 -0500

    adding crud model

--- app/models/cliente.model.js
+++ app/models/cliente.model.js
@@ -1,39 +1,107 @@
-import sql from './db';
+import sql from "./db";
 
 //Constructor
-const Cliente = function(cliente) {
-    this.email = cliente.email;
-    this.nombre = cliente.nombre;
-    this.activo = cliente.activo;
-}
+const Cliente = function (cliente) {
+  this.email = cliente.email;
+  this.nombre = cliente.nombre;
+  this.activo = cliente.activo;
+};
 
 Cliente.create = (newCliente, result) => {
-    sql.query("INSERT INTO clientes SET ?", newCliente, (err, res) => {
-        if(err){
-            console.log("error", err);
-            result(err, null);
-            return;
-        }
+  sql.query("INSERT INTO clientes SET ?", newCliente, (err, res) => {
+    if (err) {
+      console.log("error", err);
+      result(err, null);
+      return;
+    }
 
-        console.log("crear cliente: ", { id: res.insertId, ...newCliente });
-        result(null, { id: res.insertId, ...newCliente });
-    });
+    console.log("crear cliente: ", { id: res.insertId, ...newCliente });
+    result(null, { id: res.insertId, ...newCliente });
+  });
 };
 
 Cliente.findById = (clienteId, result) => {
-    sql.query("SELECT * FROM clientes WHERE id = ${clienteId}", (err, res) =>{
-        if(err){
-            console.log("error", err);
-            result(err, null);
-            return;
-        }
+  sql.query("SELECT * FROM clientes WHERE id = ${clienteId}", (err, res) => {
+    if (err) {
+      console.log("error", err);
+      result(err, null);
+      return;
+    }
 
-        if(res.length){
-            console.log("hallar cliente", res[0]);
-            result(null, res[0]);
-            return;
-        }
+    if (res.length) {
+      console.log("hallar cliente", res[0]);
+      result(null, res[0]);
+      return;
+    }
 
-        result({ kind: "cliente no encontrado" }, null);
-    })
-}
+    result({ kind: "no_encontrado" }, null);
+  });
+};
+
+Cliente.getAll = (result) => {
+  sql.query("SELECT * FROM clientes", (err, res) => {
+    if (err) {
+      console.log("error", err);
+      result(err, null);
+      return;
+    }
+
+    console.log("clientes: ", res);
+    result(null, res);
+  });
+};
+
+Cliente.updateById = (id, cliente, result) => {
+  sql.query(
+    "UPDATE clientes SET email = ?, activo = ? WHERE id = ?",
+    [cliente.email, cliente.nombre, cliente.activo, id],
+    (err, res) => {
+      if (err) {
+        console.log("error", err);
+        result(err, null);
+        return;
+      }
+
+      if (res.affectedRows == 0) {
+        result({ kind: "not_found" }, null);
+        return;
+      }
+
+      console.log("modificar cliente: ", { id: id, ...cliente });
+      result(null, { id: id, ...cliente });
+    }
+  );
+};
+
+Cliente.remove = (id, result) => {
+  sql.query("DELETE FROM clientes WHRER id = ?", id, (err, res) => {
+    if (err) {
+      console.log("error", err);
+      result(err, null);
+      return;
+    }
+
+    if (res.affectedRows == 0) {
+      result({ kind: "not_found" }, null);
+      return;
+    }
+
+    console.log("cliente borrado con el id: ", id);
+    result(null, res);
+  });
+};
+
+Cliente.removeAll = (result) => {
+  sql.query("DELETE FROM clientes", (err, res) => {
+    if (err) {
+      console.log("error", err);
+      result(err, null);
+      return;
+    }
+
+    console.log(`borrados ${res.affectedRows} clientes`);
+    result(null, res);
+  });
+};
+
+module.exports = Cliente;
