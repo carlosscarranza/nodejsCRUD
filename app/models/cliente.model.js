ccf2914965141887b460dc90a8068025ff3dee34
carlosscarranza <58747093+carlosscarranza@users.noreply.github.com>
2021-08-28 11:49:44 -0500

    add methods

--- app/models/cliente.model.js
+++ app/models/cliente.model.js
@@ -1,1 +1,39 @@
 import sql from './db';
+
+//Constructor
+const Cliente = function(cliente) {
+    this.email = cliente.email;
+    this.nombre = cliente.nombre;
+    this.activo = cliente.activo;
+}
+
+Cliente.create = (newCliente, result) => {
+    sql.query("INSERT INTO clientes SET ?", newCliente, (err, res) => {
+        if(err){
+            console.log("error", err);
+            result(err, null);
+            return;
+        }
+
+        console.log("crear cliente: ", { id: res.insertId, ...newCliente });
+        result(null, { id: res.insertId, ...newCliente });
+    });
+};
+
+Cliente.findById = (clienteId, result) => {
+    sql.query("SELECT * FROM clientes WHERE id = ${clienteId}", (err, res) =>{
+        if(err){
+            console.log("error", err);
+            result(err, null);
+            return;
+        }
+
+        if(res.length){
+            console.log("hallar cliente", res[0]);
+            result(null, res[0]);
+            return;
+        }
+
+        result({ kind: "cliente no encontrado" }, null);
+    })
+}
